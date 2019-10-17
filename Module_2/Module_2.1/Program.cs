0d7ff5707ebbaa45c5cff0ab72bbc145bc7c6b93
Mark-Palik <Mark.Palik9326@gmail.com>
2019-10-17 20:31:37 +0300

    Task 1 in module 2

---
+++ Module_2/Module_2.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2._1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number of companies, profit for each company and tax rate");
+            int compNum = int.Parse(Console.ReadLine());
+            int profit = int.Parse(Console.ReadLine());
+            int taxRate = int.Parse(Console.ReadLine());
+            Taxes(compNum, profit, taxRate);
+            Console.ReadLine();
+
+        }
+        static void Taxes(int compNum, int profit, int taxRate)
+        {
+            int totalCmpProfit = profit * compNum;
+            float stateProfit = (profit * compNum) * ((float)taxRate / 100);
+            Console.WriteLine($"state profit is : {stateProfit}");
+            Console.WriteLine("End of subtask 2.1");
+        }
+    }
+}
